7e8eb503facbd7ad0b51b21d733a22b583c4e582
Saurabh Narhe <snarhe@affinionds.com>
2019-09-26 09:59:13 +0530

    updated

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-let header_data, user_data;
-(async () => {
-  const response = await fetch(
-    "https://kfnvmcl9-dev.preview.zestyio.com/-/instant/6-92cdfaaffe-nbvzv8.json"
-  );
-  const user_details = await fetch(
-    "https://kfnvmcl9-dev.preview.zestyio.com/-/instant/6-d2cdf9b0f1-7t0gbh.json"
-  );
+const CONFIG = {
+  INSTANCE_URL: "https://kfnvmcl9-dev.preview.zestyio.com",
+  HEADER_DETAILS: "/-/instant/7-b2cbd788c8-fs1zlr.json",
+  USER_DETAILS: "/-/instant/6-d2cdf9b0f1-7t0gbh.json",
+};
 
+let header_data, user_data;
+
+(async () => {
+  
+  const header_details = await fetch(`${CONFIG.INSTANCE_URL}${CONFIG.HEADER_DETAILS}`);
+  const user_details = await fetch(`${CONFIG.INSTANCE_URL}${CONFIG.USER_DETAILS}`);
 
   user_data = await user_details.json();
-  header_data = await response.json();
-    console.log(header_data);
-  $.each(header_data.data, function(key, value) {
-    $("#dropdown").append(
-      new Option(value.content.language, value.content.language)
-    );
-  });
+  header_data = await header_details.json();
+
+  console.dir(header_data);
+  
+  // $.each(header_data.data, function(key, value) {
+  //   $("#dropdown").append(
+  //     new Option(value.content.language, value.content.language)
+  //   );
+  // });
 
   $.each(user_data.data, function(key, value) {
     // console.log(value);
@@ -23,8 +28,9 @@ let header_data, user_data;
     <td>${value.content.firstname}</td>
     <td>${value.content.lastname}</td>
     <td>${value.content.contact}</td>
-</tr>`);
+  </tr>`);
   });
+  
   render(header_data.data[0].content);
 })();
 
@@ -35,9 +41,9 @@ const render = data => {
   $('#custom').append(data.tagline);
 };
 
-$("#dropdown").change(function() {
-  const data = header_data.data.find(
-    val => val.content.language === document.getElementById("dropdown").value
-  );
-  render(data.content);
-});
+// $("#dropdown").change(function() {
+//   const data = header_data.data.find(
+//     val => val.content.language === document.getElementById("dropdown").value
+//   );
+//   render(data.content);
+// });
